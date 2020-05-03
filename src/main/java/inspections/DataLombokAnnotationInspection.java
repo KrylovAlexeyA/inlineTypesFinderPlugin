be3adf35bd8c965d5b1c7f9ad43a2d9289230c32
Alexey Krylov <kakalex208@gmail.com>
2020-05-04 00:31:13 +0300

    NoClassDefFoundError for com.intellij package fixed + DataLombokAnnotationInspection fixed and works correctly

--- src/main/java/inspections/DataLombokAnnotationInspection.java
+++ src/main/java/inspections/DataLombokAnnotationInspection.java
@@ -1,18 +1,19 @@
 package inspections;
 
 import com.intellij.codeInspection.*;
-import com.intellij.psi.*;
-import lombok.Data;
+import com.intellij.psi.PsiAnnotation;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class DataLombokAnnotationInspection extends AbstractBaseJavaLocalInspectionTool {
 
-    private static String DATA_ANNOTATION = "Data";
+    private static String DATA_ANNOTATION = "lombok.Data";
 
     @Nullable
     public ProblemDescriptor[] checkClass(@NotNull PsiClass aClass, @NotNull InspectionManager manager, boolean isOnTheFly) {
-        PsiAnnotation annotation = aClass.getAnnotation(Data.class.getName());
+        PsiAnnotation annotation = aClass.getAnnotation(DATA_ANNOTATION);
         if (annotation != null) {
             PsiFile file = aClass.getContainingFile();
             ProblemsHolder holder = new ProblemsHolder(manager, file, isOnTheFly);
