fe09cbb65bb7247cc875e1e8dacf9c85ee07b0da
Alexey Krylov <kakalex208@gmail.com>
2020-05-12 00:25:53 +0300

    LombokAnnotationInspection process only Value lombok annotation

--- src/main/java/inspections/LombokAnnotationInspection.java
+++ src/main/java/inspections/LombokAnnotationInspection.java
@@ -9,17 +9,16 @@ import org.jetbrains.annotations.Nullable;
 
 public class LombokAnnotationInspection extends AbstractBaseJavaLocalInspectionTool {
 
-    private final static String DATA_ANNOTATION = "lombok.Data";
     private final static String VALUE_ANNOTATION = "lombok.Value";
     private ClassIsSynchronizedCheck isSynchronizedCheck = new ClassIsSynchronizedCheck();
 
     @Nullable
     public ProblemDescriptor[] checkClass(@NotNull PsiClass aClass, @NotNull InspectionManager manager, boolean isOnTheFly) {
-        if ((aClass.hasAnnotation(DATA_ANNOTATION) || aClass.hasAnnotation(VALUE_ANNOTATION)) &&
-                !isSynchronizedCheck.checkMethods(aClass.getAllMethods())) {
+        if (aClass.hasAnnotation(VALUE_ANNOTATION) && !isSynchronizedCheck.checkMethods(aClass.getAllMethods())) {
             PsiFile file = aClass.getContainingFile();
             ProblemsHolder holder = new ProblemsHolder(manager, file, isOnTheFly);
-            holder.registerProblem(aClass, "Class is candidate for record/inline", ProblemHighlightType.INFORMATION);
+            holder.registerProblem(aClass, "Class is candidate for record/inline",
+                    ProblemHighlightType.INFORMATION);
             return holder.getResultsArray();
         }
         return ProblemDescriptor.EMPTY_ARRAY;
